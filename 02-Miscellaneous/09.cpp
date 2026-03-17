2f5518c70d22a2fffbf8549517f30fdd89876b77
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-17 17:36:03 +0300

    Ternary Operator.

--- 02-Miscellaneous/09.cpp
+++ 02-Miscellaneous/09.cpp
@@ -1,10 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include "My-Input-Library.h"
 
 using namespace std;
 
-int main(){
+int main()
+{
 
+
+    string Result = (MyIn::ReadNumber("Please A Enter Number:") > 0) ? "Positve" : "Nagative";
     
+    cout << Result << endl;
 
+
+    string Result2 = ((MyIn::ReadNumber("Please A Enter Number:") == 0) ? "Zero" : (MyIn::ReadNumber("Please A Enter Number:") > 0) ? "Positve" : "Nagative");
+    
+    cout << Result2 << endl;
     return 0;
 }
