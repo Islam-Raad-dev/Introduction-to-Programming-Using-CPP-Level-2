b6034cc260d1806eb264df5f621b6ee92402e1b1
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-19 14:50:47 +0300

    Automatic Variables

---
+++ 04-Variables/21.cpp
@@ -0,0 +1,25 @@
+/*
+
+Automatic Variables: المترجم هو من يحدد نوع المتغير حسب نوعية القيمة التي داخله
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    auto Number = 10;
+    auto Number1 = 10.23;
+    auto Number2 = "Islam Radd";
+
+
+    cout << Number << "\n";
+    cout << Number1 << "\n";
+    cout << Number2 << endl;
+
+    return 0;
+}
