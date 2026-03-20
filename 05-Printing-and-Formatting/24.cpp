60d130c26779cb456e7be852f4441a8fc29df741
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-20 08:47:58 +0300

    Printf: String / Char Varibales

--- 05-Printing-and-Formatting/24.cpp
+++ 05-Printing-and-Formatting/24.cpp
@@ -11,10 +11,27 @@ int main()
 {
 
     char Name[] = "Islam Raad";
-    char Letter = 'I';
+    char Letter1 = 'I';
+    char Letter2 = 'S';
+    char Letter3 = 'L';
+    char Letter4 = 'A';
+    char Letter5 = 'M';
+    char Letter6 = 'R';
+    char Letter7 = 'A';
+    char Letter8 = 'A';
+    char Letter9 = 'D';
 
     printf("Dear %s, How Are You ? \n\n", Name);
-    printf("Char = %*c\n\n", 1, Letter);
+    printf("Char = %*c\n\n", 1, Letter1);
+    printf("Char = %*c\n\n", 2, Letter2);
+    printf("Char = %*c\n\n", 3, Letter3);
+    printf("Char = %*c\n\n", 4, Letter4);
+    printf("Char = %*c\n\n", 5, Letter5);
+    printf("Char = %*c\n\n", 6, Letter6);
+    printf("Char = %*c\n\n", 7, Letter7);
+    printf("Char = %*c\n\n", 8, Letter8);        
+    printf("Char = %*c\n\n", 9, Letter9);
+
 
 
     return 0;
