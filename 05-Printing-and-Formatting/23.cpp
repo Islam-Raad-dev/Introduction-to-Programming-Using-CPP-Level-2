e0fb95fc81f63ef8c4ef54c19f6c0efdf7efc283
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-20 08:40:08 +0300

    Printf: Float / Double Varibales

---
+++ 05-Printing-and-Formatting/23.cpp
@@ -0,0 +1,20 @@
+/*
+
+Printf
+
+Float / Double Varibales
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+
+    const float PI = 3.14159256;
+
+    printf("Persction specification of = %.*f \n",1,PI);
+    printf("Persction specification of = %.*f \n",2,PI);
+    printf("Persction specification of = %.*f \n",3,PI);
+    printf("Persction specification of = %.*f \n",4,PI);
+
+    return 0;
+}
