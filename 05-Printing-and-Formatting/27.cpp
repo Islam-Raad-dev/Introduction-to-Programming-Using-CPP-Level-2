31d1191556fad78b5930431f97096fa0fb3ef122
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-22 09:27:22 +0300

    Tow Dimensional Arrays

---
+++ 05-Printing-and-Formatting/27.cpp
@@ -0,0 +1,36 @@
+/*
+
+Tow Dimensional Arrays:
+
+*/
+
+#include <iostream>
+
+int main()
+{
+
+    int Arr[10][10];
+
+    for (int i = 0; i < 10; i++)
+    {
+
+        for (int j = 0; j < 10; j++)
+        {
+
+            Arr[i][j] = (i + 1) * (j + 1);
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+
+        for (int j = 0; j < 10; j++)
+        {
+
+            printf(" %0*d ", 2, Arr[i][j]);
+        }
+
+        std::cout << std::endl;
+    }
+    return 0;
+}
