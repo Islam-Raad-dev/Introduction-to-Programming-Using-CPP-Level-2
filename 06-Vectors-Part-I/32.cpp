2db7a2617f2c2c8aa1e5fa5accd818053833b817
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-22 18:03:17 +0300

    Vector Fuctions

--- 06-Vectors-Part-I/32.cpp
+++ 06-Vectors-Part-I/32.cpp
@@ -10,4 +10,21 @@ using namespace std;
 
 int main(){
 
+    vector<int> vNumbers;
+
+    vNumbers.push_back(10);
+    vNumbers.push_back(20);
+    vNumbers.push_back(30);
+    vNumbers.push_back(40);
+    vNumbers.push_back(50);
+
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+
+
+    return 0;
+
 }
