8fe55da2346e5da2fc800e54cb854fa9699f9094
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-22 09:59:27 +0300

    Vectors: Declaration VS Intiliztion

--- 06-Vectors-Part-I/28.cpp
+++ 06-Vectors-Part-I/28.cpp
@@ -1,1 +1,26 @@
+/*
 
+Vectors: Declaration VS Intiliztion
+
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    vector<int> Numbers = {10, 20, 30, 40, 50};
+
+    cout << "Numbers Vector = ";
+
+    for (int &Num : Numbers)//Ranged loop
+    {
+        cout << Num << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
