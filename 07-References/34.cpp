3e4f21e6f55eefeb2e404405b4edf3fb772a72b5
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-23 15:34:17 +0300

    Create Refrence

--- 07-References/34.cpp
+++ 07-References/34.cpp
@@ -1,7 +1,22 @@
 /*
 
-Create Refrence 
+Create Refrence
 
 */
 #include <iostream>
 using namespace std;
+
+int main()
+{
+
+    int a = 10;
+    int & x = a;
+
+    cout << &a << endl;
+    cout << &x << endl;
+
+    cout << a << endl;
+    cout << x << endl;
+
+    return 0;
+}
