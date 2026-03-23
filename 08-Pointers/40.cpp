390d0bd7528a0e06ec7f0b9c8d0e5bfebf667f27
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-23 17:02:36 +0300

    Pointer And Arrays

---
+++ 08-Pointers/40.cpp
@@ -0,0 +1,21 @@
+/*
+
+Pointer And Arrays:
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[4] = {10, 20, 30, 40};
+    int *ptr = arr;
+
+    cout << "Addresses Are:\n";
+    cout << ptr << endl;
+    cout << ptr + 1 << endl;
+    cout << ptr + 2 << endl;
+    cout << ptr + 3 << endl;
+
+    return 0;
+}
