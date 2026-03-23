f5f4f1a5938691f1371ef9bac2c56b6a7f7f5a02
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-23 15:45:56 +0300

    Pointer

--- 08-Pointers/35.cpp
+++ 08-Pointers/35.cpp
@@ -1,12 +1,21 @@
 /*
 
-Pointer  
+Pointer : Varibal That Store Addres Another Varibal/Fuction/Object Not Value.
 
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+
+    int a = 10;
+
+    cout << a << endl;
+    cout << &a << endl;
+
+    int *p = &a;
+    cout << p << endl;
 
     return 0;
 }
