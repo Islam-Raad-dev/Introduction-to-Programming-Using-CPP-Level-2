850032649ca299866b4b65d8233d17bf094c4f9d
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-23 15:22:14 +0300

    Call ByRef / ByVal

---
+++ 07-References/33.cpp
@@ -0,0 +1,23 @@
+/*
+
+Call ByRef / ByVal
+
+*/
+#include <iostream>
+using namespace std;
+
+void Fuction1(int a)
+{
+    a++;
+}
+
+int main()
+{
+    int a = 10;
+
+    cout << a << endl; // Value In Memore in DEC / Call ByVal.
+
+    cout << &a << endl; // Adrees In Memory in HEX / Call ByRef.
+
+    return 0;
+}
