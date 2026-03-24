cd3c782d75e0806f35858d289a220b293a1ab071
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-24 07:24:40 +0300

    Vectors Access Element

---
+++ 10-Vectors-Part-II/46.cpp
@@ -0,0 +1,26 @@
+/*
+
+Vectors Access Element:
+
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    vector<int> Num = {1, 2, 3, 4, 5};
+
+    cout << "\nUsing .at(i)\n";
+    cout << "Element at Index 0 : " << Num.at(0) << endl;
+    cout << "Element at Index 2 : " << Num.at(2) << endl;
+    cout << "Element at Index 4 : " << Num.at(4) << endl;
+
+    cout << "\nUsing [i]\n";
+    cout << "Element at Index 0 : " << Num[0] << endl;
+    cout << "Element at Index 0 : " << Num[2] << endl;
+    cout << "Element at Index 0 : " << Num[3] << endl;
+
+    return 0;
+}
