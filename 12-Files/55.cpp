67a1ec20fa740c02a2f134f83e9614d9aa41ca75
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-24 11:17:08 +0300

    Files Load Data To Vecotr

---
+++ 12-Files/55.cpp
@@ -0,0 +1,46 @@
+/*
+
+Files Load Data To Vecotr :
+
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void Load_Data_From_File_To_Vector(string FileName, vector<string> &vFileContent)
+{
+
+    fstream MyFile;
+
+    MyFile.open(FileName, ios::in);
+
+    if (MyFile.is_open())
+    {
+
+        string Line;
+
+        while (getline(MyFile, Line))
+        {
+            vFileContent.push_back(Line);
+        }
+
+        MyFile.close();
+    }
+}
+
+int main()
+{
+
+    vector<string> vFileContent;
+
+    Load_Data_From_File_To_Vector("My File.txt", vFileContent);
+
+    for (string Line : vFileContent)
+    {
+        cout << Line << endl;
+    }
+
+    return 0;
+}
