4e7893de5e4afbdc0c807ed4e0ec95ff1bffab0f
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-24 08:27:53 +0300

    String Object

--- 11-Dealing-With-Strings/50.cpp
+++ 11-Dealing-With-Strings/50.cpp
@@ -1,6 +1,6 @@
 /*
 
-String Object : 
+String Object :
 
 */
 
@@ -11,7 +11,7 @@ using namespace std;
 int main()
 {
 
-    string N1 = "My Name is Islam Raadm I Love Programming";
+    string N1 = "My Name is Islam Raad, I Love Programming";
 
     //_____________________________________________
 
@@ -36,7 +36,7 @@ int main()
 
     // Insert (Mohamed) in Position 7
 
-    N1.insert(7, "Mohamed");
+    N1.insert(7, " Mohamed ");
     cout << N1 << endl;
 
     //_____________________________________________
@@ -49,7 +49,8 @@ int main()
 
     // Remove One Charecter Form The End Of The Sring
 
-    cout << N1 << endl; 
+    N1.pop_back();
+    cout << N1 << endl;
 
     //_____________________________________________
 
@@ -57,6 +58,28 @@ int main()
 
     N1.push_back('X');
     cout << N1 << endl;
+
+    //_____________________________________________
+
+    // Find ("Islam") In The String
+
+    cout << N1.find(" Mohamed ") << endl;
+
+    // If Name Did't Found:
+
+    if (N1.find("mohamed") == N1.npos)
+    {
+        cout << "Ali Is Not Found.";
+    }
+
+    //_____________________________________________
+
+    // Clear All String Letter.
+
+    N1.clear();
+    cout << N1 << endl;
+
+    //_____________________________________________
 
     return 0;
 }
