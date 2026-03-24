13379bcf90ea8177c20733522b128af62273778d
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-24 07:46:19 +0300

    Exception Handling

---
+++ 11-Dealing-With-Strings/49.cpp
@@ -0,0 +1,30 @@
+/*
+
+Exception Handling:
+
+Try / Catch.
+
+Run Time Error.
+
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    vector<int> Num = {1, 2, 3, 4, 5};
+
+    try
+    {
+        //Body. 
+        cout << Num.at(5);
+    }
+    catch (...)
+    {
+        cout << "Out Of Bound\n";
+    }
+
+    return 0;
+}
