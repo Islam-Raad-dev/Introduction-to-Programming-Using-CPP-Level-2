1b5e4bc4e80453926afe77f6a4a261d0a5740292
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-24 11:26:08 +0300

    Files Delete Record From File

---
+++ 12-Files/57.cpp
@@ -0,0 +1,43 @@
+/*
+
+Files Delete Record From File:
+
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void Save_Vector_To_File(string FileName, vector<string> vFileContent)
+{
+
+    fstream MyFile;
+
+    MyFile.open("MyFile.txt", ios::out);
+
+    if (MyFile.is_open())
+    {
+
+        for (string Line : vFileContent)
+        {
+
+            if (Line != "")
+            {
+                MyFile << Line << endl;
+            }
+        }
+
+        MyFile.close();
+    }
+}
+
+int main()
+{
+
+    vector<string> vFileContent{"Ali", "Isslam", "Ahmad"};
+
+    Save_Vector_To_File("MyFile.txt", vFileContent);
+
+    return 0;
+}
