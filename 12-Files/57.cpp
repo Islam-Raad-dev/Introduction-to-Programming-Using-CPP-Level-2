9d3c26b2e70f2934f3bd154399edfe610b1e218e
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-24 11:34:30 +0300

    Files Delete Record From File

--- 12-Files/57.cpp
+++ 12-Files/57.cpp
@@ -9,6 +9,27 @@ Files Delete Record From File:
 #include <vector>
 using namespace std;
 
+void Load_Data_From_File_To_Vector(string FileName, vector<string> &vFileContent)
+{
+
+    fstream MyFile;
+
+    MyFile.open(FileName, ios::in);
+
+    if (MyFile.is_open())
+    {
+
+        string Line;
+
+        while (getline(MyFile, Line))
+        {
+            vFileContent.push_back(Line);
+        }
+
+        MyFile.close();
+    }
+}
+
 void Save_Vector_To_File(string FileName, vector<string> vFileContent)
 {
 
@@ -32,12 +53,52 @@ void Save_Vector_To_File(string FileName, vector<string> vFileContent)
     }
 }
 
+void DeleteRecordFromFile(string FileName, string Record)
+{
+
+    vector<string> vFileContent;
+
+    Load_Data_From_File_To_Vector(FileName, vFileContent);
+
+    for (string &Line : vFileContent)
+    {
+        if (Line == Record)
+        {
+            Line = " ";
+        }
+    }
+}
+
+void PrintFile(string FileName)
+{
+
+    fstream MyFile;
+
+    MyFile.open(FileName, ios::in);
+
+    if (MyFile.is_open())
+    {
+
+        string Line;
+
+        while (getline(MyFile, Line))
+        {
+            cout << Line << endl;
+        }
+
+        MyFile.close();
+    }
+}
 int main()
 {
 
-    vector<string> vFileContent{"Ali", "Isslam", "Ahmad"};
+    cout << "File Content Before Delete\n";
+    PrintFile("MyFile.txt");
 
-    Save_Vector_To_File("MyFile.txt", vFileContent);
+    DeleteRecordFromFile("MyFile.txt", "Islam");
+
+    cout << "File Content After Delete\n";
+    PrintFile("MyFile.txt");
 
     return 0;
 }
