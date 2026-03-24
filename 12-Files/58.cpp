a7b7e454469e8522191cd1e5d9fb3611294c5adf
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-24 11:39:46 +0300

    Files Update Record From File

---
+++ 12-Files/58.cpp
@@ -0,0 +1,104 @@
+/*
+
+Files Update Record From File:
+
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void Load_Data_From_File_To_Vector(string FileName, vector<string> &vFileContent)
+{
+
+    fstream MyFile;
+
+    MyFile.open(FileName, ios::in);
+
+    if (MyFile.is_open())
+    {
+
+        string Line;
+
+        while (getline(MyFile, Line))
+        {
+            vFileContent.push_back(Line);
+        }
+
+        MyFile.close();
+    }
+}
+
+void Save_Vector_To_File(string FileName, vector<string> vFileContent)
+{
+
+    fstream MyFile;
+
+    MyFile.open("MyFile.txt", ios::out);
+
+    if (MyFile.is_open())
+    {
+
+        for (string Line : vFileContent)
+        {
+
+            if (Line != "")
+            {
+                MyFile << Line << endl;
+            }
+        }
+
+        MyFile.close();
+    }
+}
+
+void UpdateRecordFromFile(string FileName, string Record, string Update)
+{
+
+    vector<string> vFileContent;
+
+    Load_Data_From_File_To_Vector(FileName, vFileContent);
+
+    for (string &Line : vFileContent)
+    {
+        if (Line == Record)
+        {
+            Line = Update;
+        }
+    }
+}
+
+void PrintFile(string FileName)
+{
+
+    fstream MyFile;
+
+    MyFile.open(FileName, ios::in);
+
+    if (MyFile.is_open())
+    {
+
+        string Line;
+
+        while (getline(MyFile, Line))
+        {
+            cout << Line << endl;
+        }
+
+        MyFile.close();
+    }
+}
+int main()
+{
+
+    cout << "File Content Before Delete\n";
+    PrintFile("MyFile.txt");
+
+    UpdateRecordFromFile("MyFile.txt", "Islam", "Omar");
+
+    cout << "File Content After Delete\n";
+    PrintFile("MyFile.txt");
+
+    return 0;
+}
