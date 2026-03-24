ce1660fece081eb0b40aa8a69253c86062fdf344
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-03-24 08:18:17 +0300

    String Object

--- 11-Dealing-With-Strings/50.cpp
+++ 11-Dealing-With-Strings/50.cpp
@@ -1,16 +1,62 @@
 /*
 
-String Object : 
+String Object :
 
 */
 
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
+int main()
+{
 
+    string N1 = "My Name is Islam Raadm I Love Programming";
 
+    //_____________________________________________
+
+    // Length Of The String
+
+    cout << N1.length() << endl;
+
+    //_____________________________________________
+
+    // Reutrn Letter in Position 3
+
+    cout << N1.at(3) << endl;
+
+    //_____________________________________________
+
+    // Add (In C ++) To The End Of The String
+
+    N1.append("In C ++");
+    cout << N1 << endl;
+
+    //_____________________________________________
+
+    // Insert (Mohamed) in Position 7
+
+    N1.insert(7, "Mohamed");
+    cout << N1 << endl;
+
+    //_____________________________________________
+
+    // Print All The Next 8 Letter From Position 16
+
+    cout << N1.substr(16, 8) << endl;
+
+    //_____________________________________________
+
+    // Remove One Charecter Form The End Of The Sring
+
+    cout << N1.at(3) << endl;
+
+    //_____________________________________________
+
+    // Add One Charecter To The End Of The Stirng
+
+    N1.push_back('X');
+    cout << N1 << endl;
 
     return 0;
 }
